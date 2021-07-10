8c07aba9fdb09fe5b11d1e57ea811197e09ff4b3
Arushi Bhatia <ab752@duke.edu>
2021-07-10 15:04:30 -0400

    implementing api

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function textContacts() {
     body: JSON.stringify({
       phone: "9195790432",
       message: "Hello world",
-      key: "textbelt",
+      key: "4722fe44d79de99464d8437aea8875e7154670fboiE5L1hQg6rwtgWJLJrcWX3iz",
     }),
   })
     .then((response) => {
