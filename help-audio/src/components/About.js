8800c881a0bc3ec16e12082c907507bf852b0a6d
Arushi Bhatia <ab752@duke.edu>
2021-07-11 09:28:14 -0400

    added about page

--- help-audio/src/components/About.js
+++ help-audio/src/components/About.js
@@ -3,8 +3,18 @@ import '../App.css'
 
 function About() {
     return (
-        <div class="app">
-        <h1>About Page</h1>
+        <div class="app" style={{padding: "40px"}}>
+        <h1 style={{textAlign: "center"}}>About Us</h1>
+        <p>
+        50% of women feel unsafe walking alone at night. According to research conducted by YouGov RealTime, most American women are over three times as likely as men to feel unsafe doing day-to-day activities such as getting a taxi or ride-share by themselves. In the same survey, one in five women stated that they often or always feel unsafe going on a first date, while only 9% of men stated the same thing. 61% of American women also stated that they regularly take steps to avoid being sexually assaulted (Source: https://today.yougov.com/topics/lifestyle/articles-reports/2019/03/28/women-safety-sexual-assault-awareness).
+        </p>
+        <p>
+        Recently, many of us have come across videos on social media (tagged #SAFETYCALL) that are created with the intention of helping people get out of an uncomfortable and potentially dangerous situation by simulating a phone call from a loved one (example: https://www.youtube.com/watch?v=TKoWbGuRmKQ). The audio would generally go along the lines of “Hey! You’re on your way home right? Awesome, I’ll wait for you outside,” with pauses in the middle for someone to respond. This video could be saved and played by the user in any situation where they may feel unsafe, whether it’s in an Uber, walking down the street, or any other situation.
+        </p>
+        While many of us have these videos saved, we thought a lot about how if we were in a situation where we actually needed those videos, it would take too long and be too cumbersome to find a video that could be used, and it probably wouldn’t be our first instinct to do so either. In that situation, we’d probably want to text our family/friends to ask for help and to tell them that we are in a potentially unsafe situation, or would be too panicked to know what to do in the first place. 
+        <p>
+        The idea for our project came from this - we wanted to create one platform that could be used by anyone in an unsafe situation to play an audio that could deter someone from hurting them and to reach out to friends/family in a matter of seconds. While this could be used by anyone, knowing especially how many women feel unsafe in day-to-day activities, such as walking down the street, further inspired us to create this project.
+        </p>
         </div>
     );
 }
