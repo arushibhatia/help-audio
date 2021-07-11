e0470ae3637bfbef7a975c4bc68d4a5e16d7b4c3
Arushi Bhatia <ab752@duke.edu>
2021-07-11 00:19:01 -0400

    texting

--- help-audio/src/App.js
+++ help-audio/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
 import About from './components/About';
@@ -49,25 +48,54 @@ function App() {
     </div>
     </Router>
     </Container>
-     );
-   }
+  );
+}
 
-   const Home = () => (
-     <div>
-       <br/><br/>
-       <div class="btns">
-         <button id="textBtn" onClick="textContacts()">TEXT A FRIEND</button>
-        <br/>
-         <br/>
-         <button id ="fakeCall" onClick="fakeCallToUser()">FAKE CALL</button>
-        <br/>
-         <br/>
-         <button id ="locationTracker" onClick="trackLocation()">SHARE MY LOCATION</button>
-       </div>
-     </div>
-   )
+
+function Home() {
+  const textContacts = () => {
+    // get the person's contacts from the Firebase database
+    console.log("texting");
+    // turn the object (key: contact name, value: object phone number) into an array of phone numbers
+    // let allContacts = {}; // database contacts object
+    // let phoneNumArr = Object.values(allContacts);
+    // let sendString = userName + " feels like they are in danger and has auto alerted you of this via our app. Please call them and stay on the line until they are safe."
+    // for(phoneNum in phoneNumArr) {
+    //     console.log(phoneNum);
+    //  fetch("https://textbelt.com/text", {
+    //     method: "post",
+    //     headers: { "Content-Type": "application/json" },
+    //     body: JSON.stringify({
+    //       phone: phoneNum,
+    //       message: sendString,
+    //       key: "4722fe44d79de99464d8437aea8875e7154670fboiE5L1hQg6rwtgWJLJrcWX3iz",
+    //     }),
+    //   })
+    //     .then((response) => {
+    //       return response.json();
+    //     })
+    //     .then((data) => {
+    //       console.log(data);
+    //     });
+  }
+  return (
+    <div>
+    <br/><br/>
+    <div class="btns">
+      <button id="textBtn" onClick={textContacts}>TEXT A FRIEND</button>
+    <br/>
+      <br/>
+      <button id ="fakeCall" onClick="fakeCallToUser()">FAKE CALL</button>
+    <br/>
+      <br/>
+      <button id ="locationTracker" onClick="trackLocation()">SHARE MY LOCATION</button>
+    </div>
+  </div>
+  );
+} 
+
    
- 
+
 // function Home() {
 //   return (
 //     <div>
