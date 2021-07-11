df2569c0ffb87db2c7f789dbf701f90093995e3a
Arushi Bhatia <ab752@duke.edu>
2021-07-11 03:04:29 -0400

    hardcoded text

--- help-audio/src/App.js
+++ help-audio/src/App.js
@@ -17,6 +17,9 @@ import PrivateRoute from "./components/PrivateRoute"
 import ForgotPassword from "./components/ForgotPassword"
 import UpdateProfile from "./components/UpdateProfile"
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
 
 function App() {
   return (
@@ -53,30 +56,53 @@ function App() {
 
 
 function Home() {
-  const textContacts = () => {
+  async function textContacts() {
     // get the person's contacts from the Firebase database
     console.log("texting");
-    // turn the object (key: contact name, value: object phone number) into an array of phone numbers
-    // let allContacts = {}; // database contacts object
-    // let phoneNumArr = Object.values(allContacts);
-    // let sendString = userName + " feels like they are in danger and has auto alerted you of this via our app. Please call them and stay on the line until they are safe."
-    // for(phoneNum in phoneNumArr) {
-    //     console.log(phoneNum);
-    //  fetch("https://textbelt.com/text", {
-    //     method: "post",
-    //     headers: { "Content-Type": "application/json" },
-    //     body: JSON.stringify({
-    //       phone: phoneNum,
-    //       message: sendString,
-    //       key: "4722fe44d79de99464d8437aea8875e7154670fboiE5L1hQg6rwtgWJLJrcWX3iz",
-    //     }),
-    //   })
-    //     .then((response) => {
-    //       return response.json();
-    //     })
-    //     .then((data) => {
-    //       console.log(data);
-    //     });
+    console.log("texting");
+    let user = firebase.auth().currentUser;
+    console.log(user);
+    const db = firebase.firestore();
+    // const snapshot = await db.collection('users').get();
+    // snapshot.forEach((doc) => {
+    //   console.log(doc.emergencyContactName);
+    // });
+
+    db.collection('users').get().then((snapshot) => {
+      console.log(snapshot.docs)
+      snapshot.docs.forEach(doc => {
+        if(doc.data().uid === user.uid) 
+        {
+           console.log(doc.data())
+           let allContacts = doc.data().emergencyContactName; // database contacts object
+           let name =  doc.data().myName;
+           Object.keys(allContacts).forEach((contact) =>
+             {
+               let sendString = "Hey " + contact + "! " + name + " feels like they are in danger and has auto alerted you of this via our app. Please call them and stay on the line until they are safe."
+               console.log(sendString);
+               let phoneNum = allContacts[contact];
+               console.log(phoneNum);
+               fetch('https://textbelt.com/text', {
+                 method: 'post',
+                 headers: { 'Content-Type': 'application/json' },
+                 body: JSON.stringify({
+                   phone: '19195790432',
+                   message: sendString,
+                   key: 'textbelt',
+                 }),
+               }).then(response => {
+                 return response.json();
+               }).then(data => {
+                 console.log(data);
+               });
+              }
+           )
+            }
+
+        }
+      )
+
+})
   }
   return (
     <div>
